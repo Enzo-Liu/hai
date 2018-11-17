73e02f5b3880184da58517311a60a57b5acb1ba7
Liu Enze <liuenze6516@gmail.com>
2018-11-17 19:38:23 +0800

    add game board

--- src/H2048.hs
+++ src/H2048.hs
@@ -4,18 +4,22 @@ module H2048
     ) where
 
 import           Control.Monad                          (forM_, unless, when)
-import           Data.Bits                              ((.|.))
+import           Data.Bits                              ((.&.), (.|.))
 import           Data.Functor                           (void)
 import           Data.IORef                             (IORef, newIORef,
                                                          readIORef, writeIORef)
 import qualified Graphics.UI.Qtah.Core.QCoreApplication as QCoreApplication
 import qualified Graphics.UI.Qtah.Core.QEvent           as QEvent
+import qualified Graphics.UI.Qtah.Core.Types            as QType
 import           Graphics.UI.Qtah.Event
 import           Graphics.UI.Qtah.Gui.QCloseEvent       (QCloseEvent)
 import           Graphics.UI.Qtah.Signal                (connect_)
 import           Graphics.UI.Qtah.Widgets.QAction       (triggeredSignal)
 import qualified Graphics.UI.Qtah.Widgets.QAction       as QAction
+import qualified Graphics.UI.Qtah.Widgets.QBoxLayout    as QBoxLayout
 import qualified Graphics.UI.Qtah.Widgets.QFileDialog   as QFileDialog
+import qualified Graphics.UI.Qtah.Widgets.QGridLayout   as QGridLayout
+import qualified Graphics.UI.Qtah.Widgets.QLabel        as QLabel
 import           Graphics.UI.Qtah.Widgets.QMainWindow   (QMainWindow)
 import qualified Graphics.UI.Qtah.Widgets.QMainWindow   as QMainWindow
 import qualified Graphics.UI.Qtah.Widgets.QMenu         as QMenu
@@ -27,7 +31,11 @@ import           Graphics.UI.Qtah.Widgets.QTextEdit     (QTextEdit,
                                                          textChangedSignal,
                                                          undoAvailableSignal)
 import qualified Graphics.UI.Qtah.Widgets.QTextEdit     as QTextEdit
+import qualified Graphics.UI.Qtah.Widgets.QVBoxLayout   as QVBoxLayout
 import qualified Graphics.UI.Qtah.Widgets.QWidget       as QWidget
+
+
+
 import           System.FilePath                        (takeFileName)
 import           System.Random
 
@@ -41,8 +49,71 @@ makeMainWindow = do
   window <- QMainWindow.new
   QWidget.resizeRaw window 640 480
 
-  let game = newGame
+  game <- newGame
+
+  widget <- QWidget.new
+  QWidget.setStyleSheet widget "QWidget{background-color: rgb(187,173,160)}"
+  QWidget.setWindowTitle widget "h2048 by qtah"
+  QWidget.setGeometryRaw widget 100 100 500 555
+
+  drawBoard game >>= QWidget.setLayout widget
+
+  QMainWindow.setCentralWidget window widget
   return window
+
+color :: Int -> String
+color 2 ="rgb(119,110,101)"
+color 4 ="rgb(119,110,101)"
+color 0 ="rgb(0,0,0)"
+color _ = "rgb(255,255,255)"
+
+bgcolor :: Int -> String
+bgcolor 0    = "rgb(204,192,179)"
+bgcolor 2    = "rgb(238,228,218)"
+bgcolor 4    = "rgb(237,224,200)"
+bgcolor 8    = "rgb(242,177,121)"
+bgcolor 16   = "rgb(245,150,100)"
+bgcolor 32   = "rgb(245,125,95)"
+bgcolor 64   = "rgb(245,95,60)"
+bgcolor 128  = "rgb(237,207,114)"
+bgcolor 256  = "rgb(237,204,97)"
+bgcolor 512  = "rgb(237,200,80)"
+bgcolor 1024 = "rgb(210,161,68)"
+bgcolor 2048 = "rgb(237,194,46)"
+bgcolor _    = "rgb(238,228,218)"
+
+font :: Int -> String
+font 128   = "32pt"
+font 256   = "32pt"
+font 512   = "32pt"
+font 1024  = "24pt"
+font 2048  = "24pt"
+font 4096  = "24pt"
+font 8192  = "24pt"
+font 16384 = "18pt"
+font _     = "40pt"
+
+vtext :: Int -> String
+vtext 0 = ""
+vtext a = show a
+
+style :: Int -> String
+style v = "QLabel{background: " ++ bgcolor v ++
+          "; color: " ++ color v ++
+          "; font: bold; border-radius: 10px; font:" ++ font v ++
+          ";}"
+
+drawBoard :: Game -> IO QGridLayout.QGridLayout
+drawBoard (Game tiers) = do
+  gridLayout <- QGridLayout.new
+  forM_ (zip [0..] tiers) $ \(i,Tier v) -> do
+    label <- QLabel.new
+    let (r,c) = i `quotRem` 4
+    QLabel.setText label (vtext v)
+    QLabel.setAlignment label (QType.alignHCenter .|. QType.alignVCenter)
+    QWidget.setStyleSheet label (style v)
+    QGridLayout.addWidget gridLayout label r c
+  return gridLayout
 
 newGame :: IO Game
 newGame = addTier . Game . replicate 16 $ Tier 0
